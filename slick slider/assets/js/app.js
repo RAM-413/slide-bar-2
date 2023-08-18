bc586d3259cd8980a17c3129cb5873ba03043146
RAM-413 <139862416+RAM-413@users.noreply.github.com>
2023-08-18 11:30:31 +0530

    Add files via upload

---
+++ slick slider/assets/js/app.js
@@ -0,0 +1,20 @@
+$('.slide').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 200,
+    Infinity: true,
+    dots: true,
+    arrow: true,
+    prevArrow: '.prev',
+    nextArrow: '.next',
+});
+
+// $('.slide').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     adaptiveHeight: true
+// });
+
